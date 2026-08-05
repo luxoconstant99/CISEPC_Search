dcb4fa6f0aa976a4edf703dc4b7ea3cc3f8b551e
luxoconstant99 <luxoconstant99@gmail.com>
2026-08-04 23:23:38 -0400

    connect MongoDB Atlas

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-const client = new MongoClient("mongodb://127.0.0.1:27017");
-
+const client = new MongoClient(process.env.MONGODB_URI);
 async function start() {
 
     await client.connect();
